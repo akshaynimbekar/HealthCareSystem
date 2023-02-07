279c0b4086efff29916a317d0b89b85b570bc808
Satpute <pranay.satpute@capgemini.com>
2023-02-07 15:00:38 +0530

    modified Build Connection class

--- hsc/src/main/java/com/cg/hcs/controller/Test.java
+++ hsc/src/main/java/com/cg/hcs/controller/Test.java
@@ -1,7 +1,12 @@
 package com.cg.hcs.controller;
+import com.cg.hcs.connect.*;
 
+import java.sql.Connection;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.hcs.connect.JDBCConnect;
 
 @RestController
 public class Test {
@@ -10,4 +15,12 @@ public class Test {
 		public String hello() {
 			return "Hello!";
 		}
+		@GetMapping("/J")
+		public String con() {
+			JDBCConnect jd=new JDBCConnect();
+			Connection con=jd.getConnection();
+			if(con!=null)return "Connection";
+			else return "connection failed";
+		}
+		
 }
