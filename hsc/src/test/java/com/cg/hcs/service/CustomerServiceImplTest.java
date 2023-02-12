e787bca101aa3223056c437d8b342b3090314754
Yash Jaiswar <jaiswar.yash7@gmail.com>
2023-02-12 15:37:22 +0530

    JUnit Test Method Added

--- hsc/src/test/java/com/cg/hcs/service/CustomerServiceImplTest.java
+++ hsc/src/test/java/com/cg/hcs/service/CustomerServiceImplTest.java
@@ -1,9 +1,27 @@
 package com.cg.hcs.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.cg.hcs.repository.CustomerTest;
+import com.cg.hcs.service.impl.CustomerServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceImplTest {
 
+	@Mock
+	private CustomerTest customerTest;
+	
+	private CustomerServiceImpl customerService;
+	@Test
+	void makeAppointment() {
+		
+	}
+	@Test
+	void register() {
+		
+	}
+	
 }
--- hsc/src/test/java/com/cg/hcs/service/UserServiceImplTest.java
+++ hsc/src/test/java/com/cg/hcs/service/UserServiceImplTest.java
@@ -1,9 +1,25 @@
 package com.cg.hcs.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.cg.hcs.entity.User;
+import com.cg.hcs.service.impl.UserServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
 
+	@Mock
+	private User user;
+	private UserServiceImpl userServiceImpl;
+	
+	@Test
+	void addUser() {
+		
+	}
+	
+	
+	
 }
